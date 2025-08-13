29805daee8563a2d075885025b021f755c5fa225
Jacky Royer <kiky270281@gmail.com>
2025-08-13 22:09:44 +0200

    feat: lib_calc-c static version

---
+++ lib_calc-c/src/lib/staticCalc/static_calc.h
@@ -0,0 +1,11 @@
+#ifndef STATIC_CALC_H
+#define STATIC_CALC_H
+
+long double staticAdd(long double num1, long double num2);
+long double staticSubtract(long double num1, long double num2);
+long double staticMultiply(long double num1, long double num2);
+long double staticDivide(long double numerator, long double denominator);
+long double staticPower(long double base, long double exponent);
+long double staticSquareRoot(long double value);
+
+#endif
